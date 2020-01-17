63ea17fdb13525851000d6003515f864536b9b1f
ozdave <giTmnlk12>
2020-01-16 18:00:36 -0800

    Added Java logging capabilites

--- src/main/java/team6072/robot2020/Robot.java
+++ src/main/java/team6072/robot2020/Robot.java
@@ -7,17 +7,19 @@
 
 package team6072.robot2020;
 
-import edu.wpi.first.wpilibj.PIDController;
+import java.io.*;
+import java.util.logging.*;
+
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.Filesystem;
 import team6072.robot2020.commands.ArcadeDriveCmd;
 import team6072.robot2020.logging.LogWrapper;
-import team6072.robot2020.logging.SuperLogMaster;
+import team6072.robot2020.logging.JLogWrapper;
 import team6072.robot2020.logging.LogWrapper.FileType;
-import team6072.robot2020.pid.MyPIDController;
 import team6072.robot2020.subsystems.DriveSys;
 import team6072.robot2020.commands.RelativeDriveCmd;
 import team6072.robot2020.subsystems.NavXSys;
@@ -31,49 +33,80 @@ import team6072.robot2020.subsystems.NavXSys;
  */
 public class Robot extends TimedRobot {
 
-  private CommandScheduler mScheduler;
-  private LogWrapper mLog;
-  /**
-   * This function is run when the robot is first started up and should be used
-   * for any initialization code.
-   */
-  @Override
-  public void robotInit() {
-    mLog = new LogWrapper(FileType.ROBOT, "Robot", team6072.robot2020.logging.LogWrapper.Permission.ALL);
-    mScheduler = CommandScheduler.getInstance();
+	private CommandScheduler mScheduler;
+	private LogWrapper mLog;
 
-    ControlBoard.getInstance();
-    DriveSys.getInstance();
-    NavXSys.getInstance();
-  }
+	private JLogWrapper mJLog;
 
-  public void disabledInit(){
-  }
+	/**
+	 * This function is run when the robot is first started up and should be used
+	 * for any initialization code.
+	 */
+	@Override
+	public void robotInit() {
+		mLog = new LogWrapper(FileType.ROBOT, "Robot", team6072.robot2020.logging.LogWrapper.Permission.ALL);
+		// initialize the Java logging system
+		try {
+			File dir = Filesystem.getDeployDirectory();
+			String logFile = "NOTFOUND";
+			if (dir.isDirectory()) {
+				logFile = dir.getAbsolutePath() + "/logging.properties";
+			}
+			System.out.println("**********  logConfig: " + logFile + "  *********************");
+			FileInputStream configFile = new FileInputStream(logFile);
+			LogManager.getLogManager().readConfiguration(configFile);
+		} catch (IOException ex) {
+			System.out.println("WARNING: Could not open logging configuration file");
+			System.out.println("WARNING: Logging not configured (console output only)");
+		}
+		mJLog = new JLogWrapper(Robot.class.getName());
+		mLog.alarm("------  Robot initialization  ------------");
+		mScheduler = CommandScheduler.getInstance();
 
-  public void autonomousInit(){
-    mScheduler.cancelAll();
-    RelativeDriveCmd relativeDriveCmd = new RelativeDriveCmd(ControlBoard.getInstance().mJoystick0);
-    mScheduler.schedule(relativeDriveCmd);
-    mLog.alarm("Autonomous");
-    NavXSys.getInstance().resetAll();
-  }
+		ControlBoard.getInstance();
+		DriveSys.getInstance();
+		NavXSys.getInstance();
+	}
 
-  @Override
-  public void autonomousPeriodic() {
-    mScheduler.run();
-  }
+	public void disabledInit() {
+	}
 
-  public void teleopInit(){
-    mScheduler.cancelAll();
-    ArcadeDriveCmd arcadeDriveCmd = new ArcadeDriveCmd(ControlBoard.getInstance().mJoystick0);
-    mScheduler.schedule(arcadeDriveCmd);
-    mLog.alarm("TelopInit");
-    NavXSys.getInstance().resetAll();
-  }
+	/**
+	 * Autonomous code -----------------------------------------------------
+	 */
 
-  @Override
-  public void teleopPeriodic() {
-    mScheduler.run();
-  }
+	public void autonomousInit() {
+		mScheduler.cancelAll();
+		RelativeDriveCmd relativeDriveCmd = new RelativeDriveCmd(ControlBoard.getInstance().mJoystick0);
+		mScheduler.schedule(relativeDriveCmd);
+		mLog.alarm("Autonomous");
+		NavXSys.getInstance().resetAll();
+	}
+
+	@Override
+	public void autonomousPeriodic() {
+		mScheduler.run();
+	}
+
+	/**
+	 * Teleop code -------------------------------------------------------------
+	 */
+
+	public void teleopInit() {
+		mScheduler.cancelAll();
+		ArcadeDriveCmd arcadeDriveCmd = new ArcadeDriveCmd(ControlBoard.getInstance().mJoystick0);
+		mScheduler.schedule(arcadeDriveCmd);
+		mLog.alarm("TelopInit");
+		NavXSys.getInstance().resetAll();
+	}
+
+	@Override
+	public void teleopPeriodic() {
+		try {
+			mScheduler.run();
+		} catch (Exception ex) {
+			mJLog.severe(ex, "Robot.teleopPeriodic:  exception: " + ex.getMessage());
+		}
+	}
 
 }
