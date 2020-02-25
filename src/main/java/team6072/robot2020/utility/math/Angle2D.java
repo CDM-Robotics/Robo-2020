00f90244dfafb4ee7a48644b5e6f7b3245210fa0
austinleehealey <austinleehealey@gmail.com>
2020-02-24 17:53:43 -0800

    I made segments, I edited new functions for Vectors, and I made a crude path finder algorithm in the robot .java file

--- src/main/java/team6072/robot2020/utility/math/Angle2D.java
+++ src/main/java/team6072/robot2020/utility/math/Angle2D.java
@@ -1,65 +1,124 @@
 package team6072.robot2020.utility.math;
 
-/**
- * represents an Angle, can be constructed from radians, degrees, or cos and sin
- */
+
 public class Angle2D {
+    private final double m_value;
+    private final double m_cos;
+    private final double m_sin;
 
-    private double radians, degrees, cos, sin;
-
-    public static Angle2D getAngle2DFromRadians(double rad) {
-        return new Angle2D(rad);
-    }
-
-    public static Angle2D getAngle2DFromDegrees(double degrees) {
-        double rad = degrees * 2 * Math.PI / 360;
-        return new Angle2D(rad);
-    }
-
-    public static Angle2D getAngle2DFromSinCos(double sin, double cos) {
-        return new Angle2D(sin, cos);
-    }
-
+    /**
+     * Constructs a Rotation2d with a default angle of 0 degrees.
+     */
     public Angle2D() {
-        radians = 0;
-        degrees = 0;
-        cos = 1;
-        sin = 0;
+        m_value = 0.0;
+        m_cos = 1.0;
+        m_sin = 0.0;
     }
 
-    public Angle2D(double radians) {
-        this.radians = radians;
-        this.degrees = radians / (2 * Math.PI) * 360;
-        this.cos = Math.cos(this.radians);
-        this.sin = Math.sin(this.radians);
+    /**
+     * Constructs a Rotation2d with the given radian value. The x and y don't have
+     * to be normalized.
+     *
+     * @param value The value of the angle in radians.
+     */
+    public Angle2D(double value) {
+        m_value = value;
+        m_cos = Math.cos(value);
+        m_sin = Math.sin(value);
     }
 
-    public Angle2D(double sin, double cos) {
-        this.cos = cos;
-        this.sin = sin;
-        double cosRad = Math.acos(cos); // 0 to PI
-        double sinRad = Math.asin(sin);
-        if (sinRad < 0) {
-            cosRad = (2 * Math.PI) - cosRad;
+    /**
+     * Constructs a Rotation2d with the given x and y (cosine and sine) components.
+     *
+     * @param x The x component or cosine of the rotation.
+     * @param y The y component or sine of the rotation.
+     */
+    @SuppressWarnings("ParameterName")
+    public Angle2D(double x, double y) {
+        double magnitude = Math.hypot(x, y);
+        if (magnitude > 1e-6) {
+            m_sin = y / magnitude;
+            m_cos = x / magnitude;
+        } else {
+            m_sin = 0.0;
+            m_cos = 1.0;
         }
-        this.radians = cosRad;
-        this.degrees = radians / (2 * Math.PI) * 360;
+        m_value = Math.atan2(m_sin, m_cos);
     }
 
-    public double getRadians(){
-        return radians;
+    /**
+     * Constructs and returns a Rotation2d with the given degree value.
+     *
+     * @param degrees The value of the angle in degrees.
+     * @return The rotation object with the desired angle value.
+     */
+    public static Angle2D fromDegrees(double degrees) {
+        return new Angle2D(Math.toRadians(degrees));
     }
 
-    public double getDegrees(){
-        return degrees;
+
+    /*
+     * Returns the radian value of the rotation.
+     *
+     * @return The radian value of the rotation.
+     */
+    public double getRadians() {
+        return m_value;
     }
 
-    public double getCos(){
-        return cos;
+    /**
+     * Returns the degree value of the rotation.
+     *
+     * @return The degree value of the rotation.
+     */
+    public double getDegrees() {
+        return Math.toDegrees(m_value);
     }
 
-    public double getSin(){
-        return sin;
+    /**
+     * Returns the cosine of the rotation.
+     *
+     * @return The cosine of the rotation.
+     */
+    public double getCos() {
+        return m_cos;
+    }
+
+    /**
+     * Returns the sine of the rotation.
+     *
+     * @return The sine of the rotation.
+     */
+    public double getSin() {
+        return m_sin;
+    }
+
+    /**
+     * Returns the tangent of the rotation.
+     *
+     * @return The tangent of the rotation.
+     */
+    public double getTan() {
+        return m_sin / m_cos;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Rotation2d(Rads: %.2f, Deg: %.2f)", m_value, Math.toDegrees(m_value));
+    }
+
+    /**
+     * Checks equality between this Rotation2d and another object.
+     *
+     * @param obj The other object.
+     * @return Whether the two objects are equal or not.
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Angle2D) {
+            return Math.abs(((Angle2D) obj).m_value - m_value) < 1E-9;
+        }
+        return false;
     }
 
 }
--- src/main/java/team6072/robot2020/utility/math/Vector2D.java
+++ src/main/java/team6072/robot2020/utility/math/Vector2D.java
@@ -11,13 +11,27 @@ public class Vector2D {
 
     private double x, y, mag, magSquared;
 
+    public static Vector2D addVectors(Vector2D vector2d, Vector2D vector2d2) {
+        vector2d2.translateBy(vector2d);
+        return vector2d2;
+    }
+
+    public static double dotProduct(Vector2D vector2d, Vector2D vector2d2){
+        return (vector2d.getX() * vector2d2.getX() + vector2d.getY() * vector2d2.getY());
+    }
+
+    public static Vector2D scaleVector(Vector2D vector2d, double scalar){
+        return new Vector2D((vector2d.getX() * scalar), (vector2d.getY() * scalar));
+    }
+
     /**
      * This function creates a vector by using magnitude and degrees rather than an
      * x and y coordinate. This is for convienence rather than necessity.
      * 
      * @param magnitude Distance or magnitude of the vector
-     * @param degrees Degrees from zero. in Degrees
-     * @return Vector2D that represents the magnitude and direction specified by the params
+     * @param degrees   Degrees from zero. in Degrees
+     * @return Vector2D that represents the magnitude and direction specified by the
+     *         params
      */
     public static Vector2D getVectorFromMagAndDegrees(double magnitude, double degrees) {
         double rad = degrees * 2 * Math.PI / 360;
@@ -41,12 +55,13 @@ public class Vector2D {
         return new Vector2D(x, y);
     }
 
-    /** 
+    /**
      * @param x The x component of the vector
      * @param y The y component of the vector
-     * @return a Vector2D that represents the x and y commponent vectors specified by the x and y params
+     * @return a Vector2D that represents the x and y commponent vectors specified
+     *         by the x and y params
      */
-    public static Vector2D getVectorFromXAndY(double x, double y){
+    public static Vector2D getVectorFromXAndY(double x, double y) {
         return new Vector2D(x, y);
     }
 
@@ -70,7 +85,28 @@ public class Vector2D {
     }
 
     /**
+     * Applies a rotation to the translation in 2d space.
+     *
+     * This uses Linear Algebra to function... YEAHhhhhhh I'm not gonna explain it.
+     * Ask me if you REALLy need to know
+     * 
+     * This multiplies the translation vector by a counterclockwise rotation matrix
+     * of the given angle. [x_new] [other.cos, -other.sin][x] [y_new] = [other.sin,
+     * other.cos][y]
+     *
+     * For example, rotating a Translation2d of {2, 0} by 90 degrees will return a
+     * Translation2d of {0, 2}.
+     *
+     * @param angle The rotation to rotate the translation by.
+     * @return The new rotated translation.
+     */
+    public Vector2D rotateBy(Angle2D angle) {
+        return new Vector2D(x * angle.getCos() - y * angle.getSin(), x * angle.getSin() + y * angle.getCos());
+    }
+
+    /**
      * get x component of vector
+     * 
      * @return
      */
     public double getX() {
@@ -79,6 +115,7 @@ public class Vector2D {
 
     /**
      * get y component of vector
+     * 
      * @return
      */
     public double getY() {
@@ -102,6 +139,16 @@ public class Vector2D {
      */
     public double getMagSquared() {
         return magSquared;
+    }
+
+    public Vector2D inverse() {
+        return new Vector2D(-x, -y);
+    }
+
+    public Angle2D getAngle(){
+        double cos = x / getMag();
+        double sin = y / getMag();
+        return new Angle2D(sin, cos);
     }
 
 }
