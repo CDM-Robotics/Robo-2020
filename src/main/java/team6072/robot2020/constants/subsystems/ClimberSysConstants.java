13cc20b4b964bbbfe7021b7858613f1e48d048f3
ozdave <giTmnlk12>
2020-03-02 20:15:26 -0800

    WIP Climber Sys

---
+++ src/main/java/team6072/robot2020/constants/subsystems/ClimberSysConstants.java
@@ -0,0 +1,11 @@
+
+
+package team6072.robot2020.constants.subsystems;
+
+/**
+ * Add your docs here.
+ */
+public class ClimberSysConstants {
+
+    
+}
--- src/main/java/team6072/robot2020/subsystems/ClimberSys.java
+++ src/main/java/team6072/robot2020/subsystems/ClimberSys.java
@@ -1,8 +1,15 @@
 
-
 package team6072.robot2020.subsystems;
 
 import edu.wpi.first.wpilibj2.command.Subsystem;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+
+import team6072.robot2020.utility.logging.LogWrapper;
+import team6072.robot2020.constants.logging.LoggerConstants;
+import team6072.robot2020.utility.logging.LogWrapper.FileType;
+import team6072.robot2020.constants.subsystems.ClimberSysConstants;
+import team6072.robot2020.constants.subsystems.PneumaticsConstants;
 
 /**
  * Climber sys deploys a hook to attach to the seesaw bar
@@ -13,5 +20,28 @@ import edu.wpi.first.wpilibj2.command.Subsystem;
  */
 public class ClimberSys implements Subsystem {
 
+    private LogWrapper mLog;
+
+    private WPI_TalonSRX mClimberMotor;
+
+    private DoubleSolenoid mClimberSolenoid;
+
+    private static ClimberSys mClimberSys;
+
+    public static ClimberSys getInstance() {
+        if (mClimberSys == null) {
+            mClimberSys = new ClimberSys();
+        }
+        return mClimberSys;
+    }
+
+    private ClimberSys() {
+        mLog = new LogWrapper(FileType.SUBSYSTEM, "Intake", LoggerConstants.INTAKE_PERMISSION);
+
+        mClimberMotor = new WPI_TalonSRX(ClimberSysConstants.INTAKE_TALON);
+        mClimberMotor.setInverted(ClimberSysConstants.INTAKE_TALON_INVERT);
+        mClimberMotor.setNeutralMode(ClimberSysConstants.INTAKE_TALON_NEUTRAL_MODE);
+
+    }
 
 }
